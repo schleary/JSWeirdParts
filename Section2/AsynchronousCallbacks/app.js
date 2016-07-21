9cea0005f6b6db557ed71592633fe4b024122cc1
holly.leary <holly.leary@apexlearning.com>
2016-07-20 21:47:55 -0700

    Creating folder hierarchy

--- Section2/AsynchronousCallbacks/app.js
+++ Section2/AsynchronousCallbacks/app.js
@@ -1,72 +1,17 @@
-// var http = require('http');
-
-// var server = http.createServer(function(request, response) {
-//     response.writeHead(200, {
-//         'Content-type': 'text/plain'
-//     });
-//     response.end(
-
-//         "Javascript: The WEIRD PARTS\n\n" +
-
-//         "(To run, cd to this dir & run: node app.js & open IP in browser)\n\n" +
-
-//         "Section 2, Lecture 6:\n\n" +
-
-//         "Syntax Parsers:\n A program that reads your code and determines if its grammar (or syntax) is valid.\n\n" +
-
-//         "Execution Contexts (Base is Global (there can be a few contexts run)):\n A wrapper to help manage the code that is running.\n\n" +
-
-//         "Creates a Global Object (means not inside a function) and 'this.' \n\n" +
-
-//         "In the browser, the Global Object = the Window = 'this.' \n\n" +
-
-//         "INside Execution Context also exists Outer Environment (which is null at Global level, because nothing exists outside it), and Code.\n\n" +
-
-//         "Lexical Environments:\n Where something sits physically in the code you write.\n\n\n" +
-
-//         "Section 2, Lecture 7:\n\n" +
-
-//         "Name/Value Paris & Objects:\n Can only have one value in any given context, but a name van have more value/pairs.\n\n" +
-
-//         "Object: Collection of Name/Value Pairs (like JSON (says me))\n\n" 
-//     );
-// });
-
-// server.listen(8000);
-
-// console.log('Listening on http://127.0.0.1:8000');
-
-
-var a;
-
-function text() {
-    console.log(
-
-
-        "Javascript: The WEIRD PARTS\n\n" +
-
-        "(To run, cd to this dir & run: node app.js & open IP in browser)\n\n" +
-
-        "Section 2, Lecture 6:\n\n" +
-
-        "Syntax Parsers:\n A program that reads your code and determines if its grammar (or syntax) is valid.\n\n" +
-
-        "Execution Contexts (Base is Global (there can be a few contexts run)):\n A wrapper to help manage the code that is running.\n\n" +
-
-        "Creates a Global Object (means not inside a function) and 'this.' \n\n" +
-
-        "In the browser, the Global Object = the Window = 'this.' \n\n" +
-
-        "INside Execution Context also exists Outer Environment (which is null at Global level, because nothing exists outside it), and Code.\n\n" +
-
-        "Lexical Environments:\n Where something sits physically in the code you write.\n\n\n" +
-
-        "Section 2, Lecture 7:\n\n" +
-
-        "Name/Value Paris & Objects:\n Can only have one value in any given context, but a name van have more value/pairs.\n\n" +
-
-        "Object: Collection of Name/Value Pairs (like JSON (says me))\n\n" 
-
-    );
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
 }
 
+function clickHandler() {
+    console.log('click event!');   
+}
+
+// listen for the click event
+document.addEventListener('click', clickHandler);
+
+
+waitThreeSeconds();
+console.log('finished execution');
