a78f3e089014cb33c61492be70a7ab6fc96eb5e9
holly.leary <holly.leary@apexlearning.com>
2016-08-20 15:54:06 -0700

    JS is inferior because you can't have real function overloading

--- 4_ObjectsAndFunctions/FunctionOverloading/Starter/app.js
+++ 4_ObjectsAndFunctions/FunctionOverloading/Starter/app.js
@@ -1,3 +1,27 @@
+// How to deal with lack of function overloading
+
 function greet(firstname, lastname, language) {
         
+    language = language || 'en';
+    
+    if (language === 'en') {
+        console.log('Hello ' + firstname + ' ' + lastname);   
+    }
+    
+    if (language === 'es') {
+        console.log('Hola ' + firstname + ' ' + lastname);   
+    }
+    
 }
+
+
+function greetEnglish(firstname, lastname) {
+    greet(firstname, lastname, 'en');   
+}
+
+function greetSpanish(firstname, lastname) {
+    greet(firstname, lastname, 'es');   
+}
+
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
