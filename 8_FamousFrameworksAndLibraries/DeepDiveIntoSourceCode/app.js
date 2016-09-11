6e27b5d0ebe309fa1d6a286b9c7b1c660f2ae0a1
holly.leary <holly.leary@apexlearning.com>
2016-09-11 13:21:30 -0700

    Section 3 and 8

---
+++ 8_FamousFrameworksAndLibraries/DeepDiveIntoSourceCode/app.js
@@ -0,0 +1,12 @@
+var q = $("ul.people li");
+console.log(q); // jQuery.fn.inti[3] 
+// including the list of elements in the array
+// and a bunch of methods
+// and proto with all its elements
+
+// find:
+// immediately invoked functions
+// object literals
+// how it's exposed in global object
+// how is 
+console.log(q); // being used?
